62d539093b1de86fc6546c3f09d671f69eb4cf43
Estuardo Hernandez <EstuardoHernandezgt@hotmail.com>
2020-03-08 23:05:48 -0600

    Pagina de login agregad

--- frontend/terrazas/src/App.vue
+++ frontend/terrazas/src/App.vue
@@ -1,48 +1,54 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Cia Agua Logo"
+    <v-app>  
+
+
+        <v-card width="500" class="mx-auto mt-5">
+           <v-img
+          :src="require('./assets/logo.png')"
           class="my-3"
           contain
-          src="./assets/logocia.png"
-          transition="scale-transition"
-          width="60"
-        />
+          height="80"
+        />         
+        </v-card>        
+        <v-card width="500" class=mx-auto mt-5>
+            <v-card-title>               
+                <h2 class="display-1">LOGIN</h2>
+            </v-card-title>
+          <v-card-text>
+              <v-form>                  
+                  <v-text-field 
+                      label="Username" 
+                      prepend-icon="mdi-account-circle"
+                  />
+                  <v-text-field 
+                    :type="showPassword ? 'text' : 'password'"
+                    label="Password" 
+                    prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword "
+                  />
 
-
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">COMPAÑIA DE AGUA LAS TERRAZAS, S. A.</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+              </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn color="success">Register</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="info">Login</v-btn>
+            </v-card-actions>          
+        </v-card>
+    </v-app>
 </template>
+
 
 <script>
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
 </script>
